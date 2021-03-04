79509157078fe734e01cce6aebcd279f368b95c9
junghyun park <junghyun.park@wadiz.kr>
2021-03-04 23:16:00 +0900

    Change config

--- globals.ts
+++ globals.ts
@@ -1,11 +1,10 @@
 export namespace globals {
-  export const yourName = 'Alyssa P. Hacker';
-  export const siteName = 'My Awesome Blog';
+  export const yourName = 'Jeffrey';
+  export const siteName = 'Jeffrey in your area';
   export const siteDescription = 'I write about code \'n stuff';
   export const siteCreationDate = 'March 3, 2020 04:00:00 GMT';
-  export const twitterHandle = '@alyssaphacker';
-  export const email = 'alyssa@example.com';
-  export const url = 'https://alyssaphacker.com';
-  export const accentColor = '#4fc2b4';
+  export const email = 'junghyun.de@gmail.com';
+  export const url = 'https://github.com/youknowhat';
+  export const accentColor = '#0c451c';
   export const googleAnalyticsId = ``; // e.g. 'UA-999999999-1'
 }
